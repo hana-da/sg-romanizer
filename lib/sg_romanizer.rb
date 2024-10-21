f63e6e9bc8f38e231fe5a49b247cb4d10e9d32da
hana-da <hana-da@users.noreply.github.com>
2024-10-21 14:00:47 +0900

    第4回 SG Tech スキルアップ勉強会 「ローマ数字変換プログラム」
    # コードのアピールポイント
    
    - SgRomanizer#romanize も SgRomanizer#deromanize も同じような形に収められた
    - ごちゃごちゃさせずにシンプルに書けたと思う
    
    # コードを書くのにかかった時間
    
    30分ぐらい
    
    # だいたいのプログラミング歴
    
    実務経験3年ぐらい?
    
    # 実際に解いてみた感想
    
    意外とすんなり書けたので嬉しい
    
    # 伊藤さんにメッセージ（こちらももしあればw）
    
    一人開発がほとんどでコードレビューをしてもらった事がほぼないのでよろしくお願いします。

--- lib/sg_romanizer.rb
+++ lib/sg_romanizer.rb
@@ -1,9 +1,48 @@
+# frozen_string_literal: true
+
 class SgRomanizer
+  ARABIC_ROMAN = {
+    1000 => 'M',
+    900 => 'CM',
+    500 => 'D',
+    400 => 'CD',
+    100 => 'C',
+    90 => 'XC',
+    50 => 'L',
+    40 => 'XL',
+    10 => 'X',
+    9 => 'IX',
+    5 => 'V',
+    4 => 'IV',
+    1 => 'I'
+  }.freeze
+  ROMAN_ARABIC = ARABIC_ROMAN.invert.freeze
+
   def romanize(arabic)
-    # write your code here
+    roman = +''
+
+    ARABIC_ROMAN.each_key do |unit|
+      next if (i = arabic - unit).negative?
+
+      roman << ARABIC_ROMAN[unit]
+      arabic = i
+      redo
+    end
+
+    roman
   end
 
   def deromanize(roman)
-    # write your code here
+    arabic = 0
+
+    ROMAN_ARABIC.each_key do |unit|
+      next unless roman.start_with?(unit)
+
+      arabic += ROMAN_ARABIC[unit]
+      roman = roman.delete_prefix(unit)
+      redo
+    end
+
+    arabic
   end
 end
